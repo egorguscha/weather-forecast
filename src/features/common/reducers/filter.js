314c092e22cda351f8771f059941180d71333031
egorguscha <forcejmage@gmail.com>
2019-05-09 17:18:22 +0300

    added one param to inital state and one case to switch operator

--- src/features/common/reducers/filter.js
+++ src/features/common/reducers/filter.js
@@ -1,14 +1,29 @@
-import { REQUEST_CITIES, RECEIVE_CITIES } from '../action-types'
+import {
+  REQUEST_CITIES,
+  RECEIVE_CITIES,
+  FILTER_WEATHER_PARAMS
+} from '../action-types'
 
 const initialState = {
-  cities: []
+  cities: [],
+  filterType: null,
+  isLoaded: false
 }
 export const filterReducer = (state = initialState, action) => {
   switch (action.type) {
     case REQUEST_CITIES:
       return { ...state, isLoaded: action.isLoaded }
     case RECEIVE_CITIES:
-      return { ...state, isLoaded: action.isLoaded, cities: action.cities }
+      return {
+        ...state,
+        isLoaded: action.isLoaded,
+        cities: action.cities
+      }
+    case FILTER_WEATHER_PARAMS:
+      return {
+        ...state,
+        filterType: action.filterType
+      }
 
     default:
       return state
